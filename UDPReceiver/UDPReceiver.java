d3cf1eeeff25e7c078a44deee0ba11cfc70359ce
khoradan <khoradan@gmail.com>
2020-04-21 19:23:31 -0500

    Add files via upload

--- UDPReceiver/UDPReceiver.java
+++ UDPReceiver/UDPReceiver.java
@@ -1,4 +1,3 @@
-
 //Kevin Horadan
 
 import java.io.*;
@@ -13,14 +12,15 @@ public class UDPReceiver
 {
     public static void main(String argv[]) throws Exception
     {
-        final int NUM_TIME_SEND = 100;
+        final int NUM_TIME_SEND = 5;
         final int MAX_BUFFER_SIZE = 8192;//64KB - 20Byte IP Header - 8 Byte UDP Header
-        final String FILE_NAME = "text";
+        final String FILE_NAME = "StandardFile3";
         final String FILE_TYPE = ".txt";
         
+		
         int numTimes = 0;
         int numError = 0;
-        int totalTime = 0;
+        long totalTime = 0;
         int numBytes;
         long startTime, endTime, tGap;
         
@@ -29,59 +29,79 @@ public class UDPReceiver
 		BufferedWriter dumpWriter = new BufferedWriter(writer);
 		
 		FileOutputStream fileOut = new FileOutputStream("receivedFile" + numTimes + FILE_TYPE);
-        byte[] dataBuffer = new byte[MAX_BUFFER_SIZE];
-        
+        byte[] dataBuffer = new byte[MAX_BUFFER_SIZE+1];
+        byte[] ackBuffer = new byte[1];
+		
         DatagramSocket UDPsocket = new DatagramSocket(8888);
-        DatagramPacket packet = new DatagramPacket(dataBuffer, MAX_BUFFER_SIZE);
+        DatagramPacket packet = new DatagramPacket(dataBuffer, MAX_BUFFER_SIZE+1);
+		DatagramPacket ack = new DatagramPacket(ackBuffer, 1);
 
         System.out.println("Starting server. Listening for packets");
         dumpWriter.write("Starting server. Listening for packets");
 		dumpWriter.newLine();
+		
 		while(true)
         {
-            //TODO Method to check connection made
-            //timer starts here
-            //Blocks while waiting for a DatagramPacket to come in
             UDPsocket.receive(packet);
             startTime = System.currentTimeMillis();//Start time when connection made
             
             System.out.println("Starting to receive file for the " + (numTimes + 1) + "th time.");
             dumpWriter.write("Starting to receive file for the " + (numTimes + 1) + "th time.");
 			dumpWriter.newLine();
-            dataBuffer = packet.getData();
+            
+			dataBuffer = packet.getData();
             numBytes = packet.getLength();
-            DatagramPacket packetOut = new DatagramPacket(dataBuffer, numBytes, packet.getSocketAddress());
-            fileOut.write(dataBuffer, 0, numBytes);
-            UDPsocket.send(packetOut);
-            while(numBytes == MAX_BUFFER_SIZE)
+            fileOut.write(dataBuffer, 1, numBytes - 1);
+            
+			ackBuffer[0] = dataBuffer[0];
+			ack = new DatagramPacket(ackBuffer, 1, packet.getSocketAddress());
+			
+			UDPsocket.send(ack);
+			
+            while(numBytes != 0)
             {
                 UDPsocket.receive(packet);
                 dataBuffer = packet.getData();
                 numBytes = packet.getLength();
-                fileOut.write(dataBuffer, 0, numBytes);
-                UDPsocket.send(packetOut);
-            
+                
+				if(dataBuffer[0] == -1)
+				{
+					System.out.println("Received eof packet");
+					UDPsocket.send(ack);
+					break;
+					
+				}
+				if(Byte.compare(ackBuffer[0], dataBuffer[0]) != 0)
+				{
+					fileOut.write(dataBuffer, 1, numBytes - 1);
+					ackBuffer[0] = dataBuffer[0];
+					ack.setData(ackBuffer);
+				}
+				UDPsocket.send(ack);
             }
-			//fileOut.write needed?
-            fileOut.close();
-            endTime = System.currentTimeMillis();
+			
+			fileOut.close();
+            
+			endTime = System.currentTimeMillis();
             tGap = endTime - startTime;
             totalTime += tGap;
-            System.out.println("Finished receiving file for the " + (numTimes + 1) + "th time.");
+            
+			System.out.println("Finished receiving file for the " + (numTimes + 1) + "th time.");
             System.out.println("Time taken: " + tGap + "ms");
             dumpWriter.write("Finished receiving file for the " + (numTimes + 1) + "th time.");
 			dumpWriter.write("Time taken: " + tGap + "ms");
 			dumpWriter.newLine();
-            numTimes++;
+            
+			numTimes++;
             if(numTimes == NUM_TIME_SEND) break;
             fileOut = new FileOutputStream("receivedFile" + numTimes + FILE_TYPE);
         }
         UDPsocket.close();
         
         //check the received files
-        
         numError = compareFiles(FILE_NAME, FILE_TYPE, NUM_TIME_SEND);
         
+		//Output results
         System.out.println("Average time: " + (double)totalTime/numTimes + "ms.");
         System.out.println(numError + " Errors: " + (numTimes-numError) + " of " + numTimes + " files transferred correctly!");
 		dumpWriter.write("Average time: " + (double)totalTime/numTimes + "ms.");
